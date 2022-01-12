fc36dd97daccf30fff2f94995f9924f603e52a23
5gazero <kayo9410@gmail.com>
2022-01-12 23:49:14 +0900

    first setting

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Reset } from "styled-reset";
+
+function App() {
+  return (
+    <div>
+      <Reset />
+      portfolio
+    </div>
+  );
+}
+
+export default App;
