127b109e12f0d3f3771b59c2db76051833a97576
5gazero <kayo9410@gmail.com>
2022-01-17 20:25:39 +0900

    [mod]: 파일 정리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import * as React from "react";
-import { Reset } from "styled-reset";
 import GlobalStyles from "./GlobalStyles"
 
 import Header from './components/Header'
 
-function App() {
+const App = () => {
   return (
     <div>
       <GlobalStyles />
